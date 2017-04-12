611bb5b952853d16b818bac02cf189c18be73669
DreamOfFootPrints <2822850867@qq.com>
2017-04-12 13:56:56 +0800

    before
    before

--- src/main/java/com/nowcoder/aspect/LogAspect.java
+++ src/main/java/com/nowcoder/aspect/LogAspect.java
@@ -11,15 +11,17 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 /**
- * Created by nowcoder on 2016/7/10.
+ * Created by guo on 2016/7/10.
  */
 @Aspect
 @Component
 public class LogAspect {
     private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+    // //每一个类用一个日志实例维护
 
-    @Before("execution(* com.nowcoder.controller.*Controller.*(..))")
+    @Before("execution(* com.guo.controller.*Controller.*(..))")
     public void beforeMethod(JoinPoint joinPoint) {
+        // Join point：一个要被拦截的方法（在spring里面我们主要做的就是拦截方法，所以对于属性的AOP就先不理会了）
         StringBuilder sb = new StringBuilder();
         for (Object arg : joinPoint.getArgs()) {
             sb.append("arg:" + arg.toString() + "|");
@@ -27,7 +29,7 @@ public class LogAspect {
         logger.info("before method:" + sb.toString());
     }
 
-    @After("execution(* com.nowcoder.controller.IndexController.*(..))")
+    @After("execution(* com.guo.controller.IndexController.*(..))")
     public void afterMethod() {
         logger.info("after method" + new Date());
     }
